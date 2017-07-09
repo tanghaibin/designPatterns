3d84215c2f875e71bd25da7951172f20b67de97c
tanghaibin <tanghaibinhaohan@163.com>
2017-07-09 13:22:54 +0800

    update comment

--- TemplateDesign/src/top/haibin/CreateOrder.java
+++ TemplateDesign/src/top/haibin/CreateOrder.java
@@ -4,6 +4,8 @@ package top.haibin;
  * Created by haibin.tang on 2017/7/9.
  * 模板方法模式：定义程序处理流程,具体的处理逻辑由子类实现，create方法只负责调用逻辑处理
  *              模板方法模式不能替换为接口实现，因为需要在方法中定义程序的处理流程
+ *
+ *              JDK中Inputstream中就使用了模板模式，InputStream#read()
  */
 public abstract class CreateOrder {
     /**
